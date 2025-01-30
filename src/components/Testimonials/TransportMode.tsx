1c2d1c39eed60031c424bbf8160e6ec87909e0ce
UrvishChodvadiya <northpolegateway25@gmail.com>
2025-01-30 12:59:13 +0530

    add some changes in Footer and pricing section

--- src/components/Testimonials/TransportMode.tsx
+++ src/components/Testimonials/TransportMode.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+
 
 interface TransportModeProps {
   image: string;
@@ -6,18 +6,16 @@ interface TransportModeProps {
 }
 
 export default function TransportMode({ image, alt }: TransportModeProps) {
+
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.9 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 0.5 }}
-      className="relative w-full h-0 pb-[100%] md:object-contain md:w-[calc(100vw-5vw)] md:h-96 lg:w-96"
+    <div
+      className="relative w-full h-0 pb-[100%] md:object-contain md:w-[calc(100vw-5vw)] md:h-96 lg:w-[550px]"
     >
       <img
         src={image}
         alt={alt}
-        className="absolute inset-0 w-full h-full object-cover rounded-xl"
+        className={"absolute inset-0 w-full h-full object-cover rounded-xl animate-rotate"}
       />
-    </motion.div>
+    </div>
   );
 }
